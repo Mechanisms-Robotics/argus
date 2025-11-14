cd4afdfdbcafddf2077fc6146978bbc3b3808c49
Robert White <robert.white@themechanisms.org>
2025-11-13 19:37:56 -0500

    Reupdated the demo file to be a full file.

--- eink/demo/epd_2in13b_V4_test.py
+++ eink/demo/epd_2in13b_V4_test.py
@@ -49,43 +49,43 @@ try:
     epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
     time.sleep(2)
     
-    # # Drawing on the Vertical image
-    # logging.info("2.Drawing on the Vertical image...")
-    # LBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 122*250
-    # LRYimage = Image.new('1', (epd.width, epd.height), 255)  # 122*250
-    # drawblack = ImageDraw.Draw(LBlackimage)
-    # drawry = ImageDraw.Draw(LRYimage)
-    # drawblack.text((2, 0), 'hello world', font = font18, fill = 0)
-    # drawblack.text((2, 20), '2.13 epd b V4', font = font18, fill = 0)
-    # drawblack.text((20, 50), u'微雪电子', font = font18, fill = 0)
-    # drawblack.line((10, 90, 60, 140), fill = 0)
-    # drawblack.line((60, 90, 10, 140), fill = 0)
-    # drawblack.rectangle((10, 90, 60, 140), outline = 0)
-    # drawry.rectangle((10, 150, 60, 200), fill = 0)
-    # drawry.arc((15, 95, 55, 135), 0, 360, fill = 0)
-    # drawry.chord((15, 155, 55, 195), 0, 360, fill =1)
-    # epd.display(epd.getbuffer(LBlackimage), epd.getbuffer(LRYimage))
-    # time.sleep(2)
+    # Drawing on the Vertical image
+    logging.info("2.Drawing on the Vertical image...")
+    LBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 122*250
+    LRYimage = Image.new('1', (epd.width, epd.height), 255)  # 122*250
+    drawblack = ImageDraw.Draw(LBlackimage)
+    drawry = ImageDraw.Draw(LRYimage)
+    drawblack.text((2, 0), 'hello world', font = font18, fill = 0)
+    drawblack.text((2, 20), '2.13 epd b V4', font = font18, fill = 0)
+    drawblack.text((20, 50), u'微雪电子', font = font18, fill = 0)
+    drawblack.line((10, 90, 60, 140), fill = 0)
+    drawblack.line((60, 90, 10, 140), fill = 0)
+    drawblack.rectangle((10, 90, 60, 140), outline = 0)
+    drawry.rectangle((10, 150, 60, 200), fill = 0)
+    drawry.arc((15, 95, 55, 135), 0, 360, fill = 0)
+    drawry.chord((15, 155, 55, 195), 0, 360, fill =1)
+    epd.display(epd.getbuffer(LBlackimage), epd.getbuffer(LRYimage))
+    time.sleep(2)
     
-    # logging.info("3.read bmp file")
-    # Blackimage = Image.open(os.path.join(picdir, '2in13b_V4b.bmp'))
-    # RYimage = Image.open(os.path.join(picdir, '2in13b_V4b.bmp'))
-    # epd.display(epd.getbuffer(Blackimage), epd.getbuffer(RYimage))
-    # time.sleep(2)
+    logging.info("3.read bmp file")
+    Blackimage = Image.open(os.path.join(picdir, '2in13b_V4b.bmp'))
+    RYimage = Image.open(os.path.join(picdir, '2in13b_V4b.bmp'))
+    epd.display(epd.getbuffer(Blackimage), epd.getbuffer(RYimage))
+    time.sleep(2)
     
-    # logging.info("4.read bmp file on window")
-    # blackimage1 = Image.new('1', (epd.height, epd.width), 255)  # 250*122
-    # redimage1 = Image.new('1', (epd.height, epd.width), 255)  # 250*122
-    # newimage = Image.open(os.path.join(picdir, '100x100.bmp'))
-    # blackimage1.paste(newimage, (0,0))
-    # epd.display(epd.getbuffer(blackimage1), epd.getbuffer(redimage1))
+    logging.info("4.read bmp file on window")
+    blackimage1 = Image.new('1', (epd.height, epd.width), 255)  # 250*122
+    redimage1 = Image.new('1', (epd.height, epd.width), 255)  # 250*122
+    newimage = Image.open(os.path.join(picdir, '100x100.bmp'))
+    blackimage1.paste(newimage, (0,0))
+    epd.display(epd.getbuffer(blackimage1), epd.getbuffer(redimage1))
     
-    # logging.info("Clear...")
-    # epd.init()
-    # epd.clear()
+    logging.info("Clear...")
+    epd.init()
+    epd.clear()
     
-    # logging.info("Goto Sleep...")
-    # epd.sleep()
+    logging.info("Goto Sleep...")
+    epd.sleep()
         
 except IOError as e:
     logging.info(e)
